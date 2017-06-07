ee795e00774c05259b9a59693384dc6a1a4ad84a
Fernando W S Furtado <feh.wilinando@gmail.com>
2017-06-07 02:47:39 -0300

    Added controller to get IP from server

---
+++ src/main/java/br/com/caelum/treinamentos/controllers/HomeController.java
@@ -0,0 +1,25 @@
+package br.com.caelum.treinamentos.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Created by nando on 07/06/17.
+ */
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(HttpServletRequest request){
+        return request.getLocalAddr() + "\n";
+    }
+
+    @GetMapping("/busy")
+    public String busy() throws InterruptedException {
+
+        Thread.sleep(10000);
+        return "Work is finished";
+    }
+}
